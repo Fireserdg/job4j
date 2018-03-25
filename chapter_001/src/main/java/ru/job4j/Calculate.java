fec519cfddca7bafe1fec27e9b6d1bdb735ec49b
Fireserdg <serdg1984@yandex.ru>
2018-03-25 22:40:26 +0300

    1.4. JUnit

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,10 @@
 package ru.job4j;
 /**
 *Calculate
+*
 *@author Sergey Filippov (serdg1984@yandex.ru)
+*@version $Id$
+*@since 0.1
 */
 public class Calculate {
 	/**
@@ -10,5 +13,15 @@ public class Calculate {
 	*/
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+	}
+
+	/**
+	* Method echo.
+	* @param name Your name.
+	* @return Echo plus your name.
+	*/
+
+	public String echo (String name) {
+		return "Echo, echo, echo : " + name;
 	}
 }
---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,30 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author Sergey Filippov (serdg1984@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+
+public class CalculateTest {
+/**
+* Test echo.
+*/ @Test
+
+public void whenTakeNameThenTreeEchoPlusName() {
+    String input = "Sergey Filippov";
+    String expect = "Echo, echo, echo : Sergey Filippov"; 
+    Calculate calc = new Calculate();
+    String result = calc.echo(input);
+    assertThat(result, is(expect));
+}
+
+}
