b24bec218149059e0dec190ce5bcf8362d281064
Fireserdg <serdg1984@yandex.ru>
2018-03-25 22:07:49 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,13 @@
 package ru.job4j;
+/**
+*Calculate
+*@author Sergey Filippov (serdg1984@yandex.ru)
+*/
 public class Calculate {
+	/**
+	*Main.
+	*@param args - args.
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Sergey Filippov (serdg1984@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
