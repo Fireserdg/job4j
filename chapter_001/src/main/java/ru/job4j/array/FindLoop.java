f8bc2e0a4cb150bc19541274e19bc9076a3a2466
Fireserdg <serdg1984@yandex.ru>
2018-04-05 21:58:13 +0300

    5.1. Классический поиск перебором. [#33489]

---
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+/**
+ *Программа возведения числа в степень.
+ *
+ *@author Sergey Filippov (serdg1984@yandex.ru)
+ *@version $Id$
+ *@since 0.1
+ */
+public class FindLoop {
+    public int indexOf(int[] data, int el) {
+        int rst = -1;
+        for (int index = 0; index < data.length; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
+}
