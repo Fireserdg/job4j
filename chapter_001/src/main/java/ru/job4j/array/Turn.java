e6f93adf746271de7f3ba738f2b9bef2ac5deab8
Fireserdg <serdg1984@yandex.ru>
2018-04-07 16:24:37 +0300

    5.2. Перевернуть массив. [#4441]

---
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,21 @@
+package ru.job4j.array;
+/**
+ *Перевернуть массив.
+ *
+ *@author Sergey Filippov (serdg1984@yandex.ru)
+ *@version $Id$
+ *@since 0.1
+ */
+public class Turn {
+    public int[] turn(int[] array) {
+
+        for (int i =0; i < array.length/2; i++) {
+            int a= array [i];
+            array [i] = array [array.length - i -1];
+            array [array.length - i -1] = a;
+        }
+        return array;
+    }
+
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/TurnTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+/**
+ *@author Sergey Filippov (serdg1984@yandex.ru)
+ *@version $Id$
+ *@since 0.1
+ */
+public class TurnTest {
+    @Test
+    public void whenTurnArrayWithEvenAmountOfElementsThenTurnedArray() {
+        Turn turner = new Turn();
+        int[] input = new int[] {1, 2, 3, 4, 5};
+        int[] result = turner.turn(input);
+        int[] expect = new int[] {5, 4, 3, 2, 1};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenTurnArrayWithOddAmountOfElementsThenTurnedArray2() {
+        Turn turner = new Turn();
+        int[] input = new int[] {4, 1, 6, 2, 7, 9};
+        int[] result = turner.turn(input);
+        int[] expect = new int[] {9, 7, 2, 6, 1, 4};
+        assertThat(result, is(expect));
+    }
+}
