f94bef624580bac0502182da078a95ad04f2f5a5
Fireserdg <serdg1984@yandex.ru>
2018-04-08 10:49:48 +0300

    5.6. Удаление дубликатов в массиве. [#225]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -0,0 +1,31 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+/**
+ *Удаление дубликатов в массиве.
+ *
+ *@author Sergey Filippov (serdg1984@yandex.ru)
+ *@version $Id$
+ *@since 0.1
+ */
+public class ArrayDuplicate {
+    /**
+     * Drop duplicates;
+     * @param array;
+     * @return without duplicates;
+     */
+    public String[] remove(String[] array) {
+        int unique = array.length;
+        for (int i = 0; i < unique; i++) {
+            for (int j = i + 1; j < unique; j++) {
+                if (array[i].equals(array[j])) {
+                    array[j] = array[unique - 1];
+                    unique--;
+                    j--;
+                }
+            }
+        }
+        return Arrays.copyOf(array, unique);
+    }
+}
