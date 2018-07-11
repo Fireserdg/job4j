7c09b4cd525c8d97ceb7ec450456349e2fe6fb3a
Fireserdg <serdg1984@yandex.ru>
2018-07-11 23:50:21 +0300

    5. Рефакторинг теста @Before @After [#33568]

--- chapter_002/src/test/java/ru/job4j/patternstrategy/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/patternstrategy/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.patternstrategy;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -13,11 +15,23 @@ import static org.junit.Assert.*;
  * @since 11.07.2018.
  */
 public class PaintTest {
+    //поле содержит дефолтный вывод на консоль.
+    private final PrintStream stdout = System.out;
+    //буфер результата.
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         assertThat(new String(out.toByteArray()), is(new StringBuilder()
                                                     .append("++++")
@@ -26,13 +40,9 @@ public class PaintTest {
                                                     .append("++++")
                                                     .append(System.lineSeparator())
                                                     .toString()));
-        System.setOut(stdout);
     }
     @Test
     public void whenDrawTriangle() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Triangle());
         assertThat(new String(out.toByteArray()), is(new StringBuilder()
                                                     .append("  +  ")
@@ -40,7 +50,6 @@ public class PaintTest {
                                                     .append("+++++")
                                                     .append(System.lineSeparator())
                                                     .toString()));
-        System.setOut(stdout);
     }
 
 }
