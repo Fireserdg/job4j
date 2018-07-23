133aa35f4234408bcf48c2316ea3588047aba21e
Fireserdg <serdg1984@yandex.ru>
2018-07-23 23:17:43 +0300

    Крестики-нолики на JavaFX [#49777][#61713] (Исправлено)

--- chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
+++ chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -11,101 +11,76 @@ public class Logic3T {
     }
 
     public boolean isWinnerX() {
-            return (table[0][0].hasMarkX() && table[1][1].hasMarkX() && table[2][2].hasMarkX())
-                    || (table[0][2].hasMarkX() && table[1][1].hasMarkX() && table[2][0].hasMarkX())
-                    || (table[0][0].hasMarkX() && table[0][1].hasMarkX() && table[0][2].hasMarkX())
-                    || (table[0][0].hasMarkX() && table[1][0].hasMarkX() && table[2][0].hasMarkX())
-                    || (table[0][1].hasMarkX() && table[1][1].hasMarkX() && table[2][1].hasMarkX())
-                    || (table[0][2].hasMarkX() && table[1][2].hasMarkX() && table[2][2].hasMarkX())
-                    || (table[1][0].hasMarkX() && table[1][1].hasMarkX() && table[1][2].hasMarkX())
-                    || (table[2][0].hasMarkX() && table[2][1].hasMarkX() && table[2][2].hasMarkX());
+        boolean result = false;
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table.length; j++) {
+                int count = 0;
+                if (table[i][j].hasMarkX()) {
+                    if (table[j][count].hasMarkX() && table[j][count + 1].hasMarkX()
+                            && table[j][count + 2].hasMarkX()) {
+                        result = true;
+                        break;
+                    } else if (table[count][j].hasMarkX() && table[count + 1][j].hasMarkX()
+                            && table[count + 2][j].hasMarkX()) {
+                        result = true;
+                        break;
+                    } else if (table[count][count].hasMarkX() && table[count + 1][count + 1].hasMarkX()
+                            && table[count + 2][count + 2].hasMarkX()) {
+                        result = true;
+                        break;
+                    } else if (table[count + 2][count].hasMarkX() && table[count + 1][count + 1].hasMarkX()
+                            && table[count][count + 2].hasMarkX()) {
+                        result = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
     }
 
     public boolean isWinnerO() {
-        return (table[0][0].hasMarkO() && table[1][1].hasMarkO() && table[2][2].hasMarkO())
-                || (table[0][2].hasMarkO() && table[1][1].hasMarkO() && table[2][0].hasMarkO())
-                || (table[0][0].hasMarkO() && table[0][1].hasMarkO() && table[0][2].hasMarkO())
-                || (table[0][0].hasMarkO() && table[1][0].hasMarkO() && table[2][0].hasMarkO())
-                || (table[0][1].hasMarkO() && table[1][1].hasMarkO() && table[2][1].hasMarkO())
-                || (table[0][2].hasMarkO() && table[1][2].hasMarkO() && table[2][2].hasMarkO())
-                || (table[1][0].hasMarkO() && table[1][1].hasMarkO() && table[1][2].hasMarkO())
-                || (table[2][0].hasMarkO() && table[2][1].hasMarkO() && table[2][2].hasMarkO());
+        boolean result = false;
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table.length; j++) {
+                int count = 0;
+                if (table[i][j].hasMarkO()) {
+                    if (table[j][count].hasMarkO() && table[j][count + 1].hasMarkO()
+                            && table[j][count + 2].hasMarkO()) {
+                        result = true;
+                        break;
+                    } else if (table[count][j].hasMarkO() && table[count + 1][j].hasMarkO()
+                            && table[count + 2][j].hasMarkO()) {
+                        result = true;
+                        break;
+                    } else if (table[count][count].hasMarkO() && table[count + 1][count + 1].hasMarkO()
+                            && table[count + 2][count + 2].hasMarkO()) {
+                        result = true;
+                        break;
+                    } else if (table[count + 2][count].hasMarkO() && table[count + 1][count + 1].hasMarkO()
+                            && table[count][count + 2].hasMarkO()) {
+                        result = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
     }
 
     public boolean hasGap() {
         boolean result = true;
-        if (table[0][0].hasMarkX() && table[0][1].hasMarkO() && table[0][2].hasMarkO()
-                && table[1][0].hasMarkX() && table[1][1].hasMarkO() && table[1][2].hasMarkX()
-                && table[2][0].hasMarkO() && table[2][1].hasMarkX() && table[2][2].hasMarkO()) {
-            result = false;
-        } else if (table[0][0].hasMarkX() && table[0][1].hasMarkX() && table[0][2].hasMarkO()
-                && table[1][0].hasMarkO() && table[1][1].hasMarkO() && table[1][2].hasMarkX()
-                && table[2][0].hasMarkX() && table[2][1].hasMarkO() && table[2][2].hasMarkX()) {
-            result = false;
-        } else if (table[0][0].hasMarkX() && table[0][1].hasMarkX() && table[0][2].hasMarkO()
-                && table[1][0].hasMarkO() && table[1][1].hasMarkO() && table[1][2].hasMarkX()
-                && table[2][0].hasMarkX() && table[2][1].hasMarkX() && table[2][2].hasMarkO()) {
-            result = false;
-        } else if (table[0][0].hasMarkX() && table[0][1].hasMarkO() && table[0][2].hasMarkX()
-                && table[1][0].hasMarkO() && table[1][1].hasMarkO() && table[1][2].hasMarkX()
-                && table[2][0].hasMarkX() && table[2][1].hasMarkX() && table[2][2].hasMarkO()) {
-            result = false;
-        } else if (table[0][0].hasMarkO() && table[0][1].hasMarkX() && table[0][2].hasMarkX()
-                && table[1][0].hasMarkX() && table[1][1].hasMarkX() && table[1][2].hasMarkO()
-                && table[2][0].hasMarkO() && table[2][1].hasMarkO() && table[2][2].hasMarkX()) {
-            result = false;
-        } else if (table[0][0].hasMarkX() && table[0][1].hasMarkO() && table[0][2].hasMarkX()
-                && table[1][0].hasMarkO() && table[1][1].hasMarkX() && table[1][2].hasMarkX()
-                && table[2][0].hasMarkO() && table[2][1].hasMarkX() && table[2][2].hasMarkO()) {
-            result = false;
-        } else if (table[0][0].hasMarkO() && table[0][1].hasMarkO() && table[0][2].hasMarkX()
-                && table[1][0].hasMarkX() && table[1][1].hasMarkX() && table[1][2].hasMarkO()
-                && table[2][0].hasMarkO() && table[2][1].hasMarkX() && table[2][2].hasMarkX()) {
-            result = false;
-        } else if (table[0][0].hasMarkO() && table[0][1].hasMarkX() && table[0][2].hasMarkO()
-                && table[1][0].hasMarkX() && table[1][1].hasMarkO() && table[1][2].hasMarkX()
-                && table[2][0].hasMarkX() && table[2][1].hasMarkO() && table[2][2].hasMarkX()) {
-            result = false;
-        } else if (table[0][0].hasMarkX() && table[0][1].hasMarkO() && table[0][2].hasMarkX()
-                && table[1][0].hasMarkX() && table[1][1].hasMarkX() && table[1][2].hasMarkO()
-                && table[2][0].hasMarkO() && table[2][1].hasMarkX() && table[2][2].hasMarkO()) {
-            result = false;
-        } else if (table[0][0].hasMarkO() && table[0][1].hasMarkX() && table[0][2].hasMarkX()
-                && table[1][0].hasMarkX() && table[1][1].hasMarkO() && table[1][2].hasMarkO()
-                && table[2][0].hasMarkO() && table[2][1].hasMarkX() && table[2][2].hasMarkX()) {
-            result = false;
-        } else if (table[0][0].hasMarkX() && table[0][1].hasMarkX() && table[0][2].hasMarkO()
-                && table[1][0].hasMarkO() && table[1][1].hasMarkX() && table[1][2].hasMarkX()
-                && table[2][0].hasMarkX() && table[2][1].hasMarkO() && table[2][2].hasMarkO()) {
-            result = false;
-        } else if (table[0][0].hasMarkX() && table[0][1].hasMarkO() && table[0][2].hasMarkX()
-                && table[1][0].hasMarkX() && table[1][1].hasMarkO() && table[1][2].hasMarkO()
-                && table[2][0].hasMarkO() && table[2][1].hasMarkX() && table[2][2].hasMarkX()) {
-            result = false;
-        } else if (table[0][0].hasMarkX() && table[0][1].hasMarkO() && table[0][2].hasMarkX()
-                && table[1][0].hasMarkX() && table[1][1].hasMarkO() && table[1][2].hasMarkX()
-                && table[2][0].hasMarkO() && table[2][1].hasMarkX() && table[2][2].hasMarkO()) {
-            result = false;
-        } else if (table[0][0].hasMarkX() && table[0][1].hasMarkO() && table[0][2].hasMarkX()
-                && table[1][0].hasMarkX() && table[1][1].hasMarkO() && table[1][2].hasMarkX()
-                && table[2][0].hasMarkO() && table[2][1].hasMarkX() && table[2][2].hasMarkO()) {
-            result = false;
-        } else if (table[0][0].hasMarkO() && table[0][1].hasMarkX() && table[0][2].hasMarkX()
-                && table[1][0].hasMarkX() && table[1][1].hasMarkO() && table[1][2].hasMarkO()
-                && table[2][0].hasMarkX() && table[2][1].hasMarkO() && table[2][2].hasMarkX()) {
-            result = false;
-        } else if (table[0][0].hasMarkO() && table[0][1].hasMarkX() && table[0][2].hasMarkO()
-                && table[1][0].hasMarkO() && table[1][1].hasMarkX() && table[1][2].hasMarkX()
-                && table[2][0].hasMarkX() && table[2][1].hasMarkO() && table[2][2].hasMarkX()) {
-            result = false;
-        } else if (table[0][0].hasMarkO() && table[0][1].hasMarkX() && table[0][2].hasMarkO()
-                && table[1][0].hasMarkO() && table[1][1].hasMarkX() && table[1][2].hasMarkO()
-                && table[2][0].hasMarkX() && table[2][1].hasMarkO() && table[2][2].hasMarkX()) {
-            result = false;
-        } else if (table[0][0].hasMarkO() && table[0][1].hasMarkX() && table[0][2].hasMarkO()
-                && table[1][0].hasMarkO() && table[1][1].hasMarkX() && table[1][2].hasMarkX()
-                && table[2][0].hasMarkX() && table[2][1].hasMarkO() && table[2][2].hasMarkO()) {
-            result = false;
+        int count = 0;
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table.length; j++) {
+                if (table[i][j].hasMarkO() || table[i][j].hasMarkX()) {
+                    count++;
+                    if (count == table.length * table[0].length) {
+                        result = false;
+                        break;
+                    }
+                }
+            }
         }
         return result;
     }
--- chapter_001/src/test/java/ru/job4j/tictactoe/Logic3TTest.java
+++ chapter_001/src/test/java/ru/job4j/tictactoe/Logic3TTest.java
@@ -54,16 +54,16 @@ public class Logic3TTest {
     @Test
     public void whenHasGas() {
         Figure3T[][] table = {
-                {new Figure3T(true), new Figure3T(false), new Figure3T(true)},
+                {new Figure3T(), new Figure3T(false), new Figure3T(true)},
                 {new Figure3T(true), new Figure3T(false), new Figure3T(false)},
-                {new Figure3T(false), new Figure3T(true), new Figure3T(true)},
+                {new Figure3T(false), new Figure3T(true), new Figure3T()},
         };
         Logic3T login = new Logic3T(table);
-        assertThat(login.hasGap(), is(false));
+        assertThat(login.hasGap(), is(true));
     }
 
     @Test
-    public void whenHasGasSecondTime() {
+    public void whenNotHasGas() {
         Figure3T[][] table = {
                 {new Figure3T(false), new Figure3T(true), new Figure3T(true)},
                 {new Figure3T(true), new Figure3T(false), new Figure3T(false)},
