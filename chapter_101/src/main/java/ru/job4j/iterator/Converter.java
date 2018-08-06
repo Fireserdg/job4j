c115ac1234b39b48e2f510bda10ddd97310ee1fd
Fireserdg <serdg1984@yandex.ru>
2018-08-06 17:26:57 +0300

    5.1.4. Создать convert(Iterator<Iterator>) [#152]

---
+++ chapter_101/src/main/java/ru/job4j/iterator/Converter.java
@@ -0,0 +1,54 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Iterator for Even values.
+ *
+ * @author Sergey Filippov (serdg1984@yandex.ru).
+ * @version $Id$.
+ * @since 06.08.2018.
+ */
+public class Converter {
+
+    /**
+     * Convert several Iterators to one.
+     *
+     * @param it several Iterators
+     * @return new Iterator with all the values.
+     */
+    public Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
+        return new Iterator<Integer>() {
+            private Iterator<Integer> iterator = it.next();
+            /**
+             * Checks possible increment.
+             *
+             * @return booleans value.
+             */
+            @Override
+            public boolean hasNext() {
+                return this.iterator.hasNext();
+            }
+
+            /**
+             * Pointer to the element.
+             *
+             * @return values.
+             */
+            @Override
+            public Integer next() {
+                int result;
+                if (this.iterator.hasNext()) {
+                    result = this.iterator.next();
+                } else {
+                    throw new NoSuchElementException();
+                }
+                if (!(this.iterator.hasNext()) && it.hasNext()) {
+                    this.iterator = it.next();
+                }
+                return result;
+            }
+        };
+    }
+}
