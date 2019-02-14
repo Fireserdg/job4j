2dabc017c0edf2dbae549196790b0218711b58bc
Fireserdg <serdg1984@yandex.ru>
2019-02-14 23:24:00 +0300

    change package info

--- chapter_201/todo_list/src/main/java/ru/job4j/todo/package-info.java
+++ chapter_201/todo_list/src/main/java/ru/job4j/todo/package-info.java
@@ -5,17 +5,4 @@
  * @version 1.0.
  * @since 2019-02-11
  */
-@org.hibernate.annotations.NamedQueries({
-        @org.hibernate.annotations.NamedQuery(
-                name = "findItemById",
-                query = "select i from item as i where i.id=:id"
-        ),
-
-        @org.hibernate.annotations.NamedQuery(
-                name = "DeleteItem",
-                query = "delete from items where id=:id",
-                comment = "custom SQL comment"
-        ),
-})
-
 package ru.job4j.todo;
